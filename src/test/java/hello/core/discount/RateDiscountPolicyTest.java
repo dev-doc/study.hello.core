2a9ae343ee561a1dd9a119511d11182d3d719a50
dev-doc <doc@kakao.com>
2022-07-03 01:53:28 +0900

    AppConfig 추가

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -1,13 +1,16 @@
 package hello.core.discount;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 class RateDiscountPolicyTest {
+
     RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Test
@@ -22,14 +25,14 @@ class RateDiscountPolicyTest {
     }
 
     @Test
-    @DisplayName("VIP가 아니면 할인율이 없다")
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
     void vip_x() {
         //given
         Member member = new Member(1L, "memberBasic", Grade.BASIC);
         //when
         int discount = discountPolicy.discount(member, 10000);
         //then
-        assertThat(discount).isEqualTo(0);
+        assertThat(discount).isZero();
     }
 
 }
