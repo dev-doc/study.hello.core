92ab11de2ba24e7545e1b1d1e4a3f4339f8fcb26
dev-doc <doc@kakao.com>
2022-07-03 17:00:38 +0900

    스프링 컨테이너와 스프링 빈

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -5,6 +5,8 @@ import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class MemberServiceTest {
 
@@ -12,8 +14,11 @@ class MemberServiceTest {
 
     @BeforeEach
     public void beforeEach() {
-        AppConfig appConfig = new AppConfig();
-        memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        memberService = applicationContext.getBean("memberService", MemberService.class);
+
+//        AppConfig appConfig = new AppConfig();
+//        memberService = appConfig.memberService();
     }
 
     @Test
