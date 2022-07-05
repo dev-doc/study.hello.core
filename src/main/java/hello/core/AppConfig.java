cb0539023d8cb5efd79c3041836ab04b701d4d47
dev-doc <doc@kakao.com>
2022-07-05 23:03:04 +0900

    스프링 컨테이너와 스프링 빈

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,21 +14,25 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        System.out.println("Call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("CAll AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("Call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public RateDiscountPolicy discountPolicy() {
+        System.out.println("Call AppConfig.discountPolicy");
         return new RateDiscountPolicy();
     }
 
